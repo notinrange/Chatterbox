6c939a6e8daab6488f3b3fabf6bbe1114a5ee8cf
rahulsl73 <rahul.kumar@splashlern.com>
2025-12-12 18:07:16 +0530

    configured

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,43 @@
+import "package:flutter/material.dart";
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 1,
+        title: Text('ChatterBox',
+          style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.normal,
+                fontSize: 19,
+              ),
+        ),
+        backgroundColor: Colors.white,
+        ),
+    );
+  }
+}
